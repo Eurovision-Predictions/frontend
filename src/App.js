f27cfe40f88ab134ff4730bdb055ecc334188b1d
DictumMortuum <theio.vrefos@gmail.com>
2022-05-11 14:25:31 +0300

    added theme options

--- src/App.js
+++ src/App.js
@@ -6,10 +6,30 @@ import GroupPage from './GroupPage';
 import LandingPage from './LandingPage';
 import PredictionsPage from './PredictionsPage';
 import AppBar from './AppBar';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#16213d',
+    },
+    secondary: {
+      main: '#eea2a2',
+    },
+    info: {
+      main: '#e8dbb4',
+    },
+    success: {
+      main: '#92acdf',
+    },
+    divider: 'rgb(115, 83, 86)',
+  },
+});
 
 const App = () => {
   return (
-    <React.Fragment>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <AppBar />
       <Container maxWidth="lg">
@@ -19,7 +39,7 @@ const App = () => {
           <Route path="/" element={<PredictionsPage />} />
         </Routes>
       </Container>
-    </React.Fragment>
+    </ThemeProvider>
   );
 }
 
